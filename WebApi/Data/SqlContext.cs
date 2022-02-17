11162e147d06f1a4c511a83799951319242ff76f
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-02-17 12:10:36 +0100

    Add project files.

---
+++ WebApi/Data/SqlContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models.Entities;
+
+namespace WebApi.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+
+        }
+
+        public SqlContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<CategoryEntity> Categories { get; set; }
+        public virtual DbSet<ProductEntity> Products { get; set; }
+    }
+}
---
+++ WebApi/Models/Entities/ProductEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models.Entities
+{
+    public class ProductEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required, Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public CategoryEntity Category { get; set; }
+    }
+}
